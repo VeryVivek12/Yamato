cca09bd12cbfd010d0f046ffc0410c661d1f6d00
Vivek Topiya <vivektopiya12@gmail.com>
2019-09-04 22:14:24 +0530

    Initial Commit

---
+++ Yamato/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yamato
+//
+//  Created by Vivek Topiya on 04/09/19.
+//  Copyright © 2019 Vivek Topiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
